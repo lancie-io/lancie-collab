8bf42c9c1dc226da2472e4f63fbf0d032e830e35
lukasjoho <social@lukashoppe.com>
2024-02-07 13:27:46 +0100

    fix: remove unused comment components

--- components/project/elements/videos/VideoGallery.tsx
+++ components/project/elements/videos/VideoGallery.tsx
@@ -1,10 +1,6 @@
 'use client';
 import { useCreateThread, useThreads } from '@/liveblocks.config';
-import {
-  Comment,
-  Composer,
-  ComposerSubmitComment,
-} from '@liveblocks/react-comments';
+import { ComposerSubmitComment } from '@liveblocks/react-comments';
 import { FormEvent, useCallback, useMemo, useState } from 'react';
 import ReactPlayer from 'react-player';
 import { BuilderElementInstance } from '../../BuilderElements';
@@ -65,6 +61,7 @@ const VideoItem = ({ video }: VideoItemProps) => {
         metadata: {
           type: 'video-thread',
           id: video,
+          resolved: false,
         },
       });
     },
@@ -89,26 +86,7 @@ const VideoItem = ({ video }: VideoItemProps) => {
       </div>
       <div className="col-span-5 border-l aspect-square relative">
         <div className="absolute w-full h-full left-0 top-0 overflow-scroll p-4 gap-4 flex flex-col pb-12">
-          <Composer
-            className="bg-muted border rounded-md"
-            onComposerSubmit={handleSubmit}
-          />
-          {sortedThreads.map((thread) => {
-            return (
-              <div className="border rounded-md" key={thread.id}>
-                {thread.comments.map((comment) => (
-                  <Comment
-                    showActions={true}
-                    showReactions={true}
-                    key={comment.id}
-                    comment={comment}
-                    className="bg-muted"
-                  />
-                ))}
-                <Composer className="bg-muted border-t" threadId={thread.id} />
-              </div>
-            );
-          })}
+          DEV: TBD
         </div>
         <div className="w-full h-12 bg-gradient-to-t from-background to-transparent bottom-0 absolute left-0" />
       </div>
