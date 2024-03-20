6cfefffa9a9dbf2ad027659a7fe8abb4cc26ad5e
lukasjoho <social@lukashoppe.com>
2024-03-20 20:45:33 +0100

    build: add aws provider

--- app/api/edgestore/[...edgestore]/route.ts
+++ app/api/edgestore/[...edgestore]/route.ts
@@ -1,17 +1,20 @@
 import { initEdgeStore } from '@edgestore/server';
 import { createEdgeStoreNextHandler } from '@edgestore/server/adapters/next/app';
+import { AWSProvider } from '@edgestore/server/providers/aws';
+
 const es = initEdgeStore.create();
-/**
- * This is the main router for the Edge Store buckets.
- */
+
 const edgeStoreRouter = es.router({
   publicFiles: es.fileBucket(),
 });
 const handler = createEdgeStoreNextHandler({
+  provider: AWSProvider({
+    accessKeyId: process.env.APP_AWS_ACCESS_KEY,
+    secretAccessKey: process.env.APP_AWS_SECRET_KEY,
+    region: process.env.APP_AWS_REGION,
+    bucketName: process.env.NEXT_PUBLIC_AWS_S3_BUCKET_NAME,
+  }),
   router: edgeStoreRouter,
 });
 export { handler as GET, handler as POST };
-/**
- * This type is used to create the type-safe client for the frontend.
- */
 export type EdgeStoreRouter = typeof edgeStoreRouter;
