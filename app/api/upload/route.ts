97ae484971d6003a00f08746914aa2069cca6fe7
lukasjoho <social@lukashoppe.com>
2024-03-21 23:14:17 +0100

    fix: change env name

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -20,7 +20,7 @@ export async function GET(request: Request) {
 
     const currentTimeInSeconds = Math.floor(new Date().getTime() / 1000);
     const post = await s3.createPresignedPost({
-      Bucket: process.env.NEXT_PUBLIC_AWS_S3_BUCKET_NAME,
+      Bucket: process.env.AWS_S3_BUCKET_NAME,
       Fields: {
         key: `experiments/${currentTimeInSeconds}-${filename}`,
       },
